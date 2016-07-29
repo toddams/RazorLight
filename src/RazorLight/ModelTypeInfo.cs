0f442fa934ce200f90c182caf9017f68512d57ba
user <user@IVAN>
2016-07-29 19:08:16 +0300

    Rewrite modeltypeInfo

--- src/RazorLight/ModelTypeInfo.cs
+++ src/RazorLight/ModelTypeInfo.cs
@@ -6,11 +6,14 @@ namespace RazorLight
 {
 	public class ModelTypeInfo
 	{
+		public bool IsStrongType { get; private set; }
 		public Type Type { get; private set; }
-		public bool IsAnonymousType { get; private set; }
-		public string TemplateTypeName
+		public Type TemplateType { get; private set; }
+		public string TemplateTypeName { get; private set; }
+
+		public object CreateTemplateModel(object model)
 		{
-			get { return IsAnonymousType ? "dynamic" : Type.FullName; }
+			return this.IsStrongType ? model : model.ToExpando();
 		}
 
 		public ModelTypeInfo(Type type)
@@ -21,22 +24,9 @@ namespace RazorLight
 			}
 
 			this.Type = type;
-			this.IsAnonymousType = type.IsAnonymousType();
+			this.IsStrongType = type != typeof(ExpandoObject) && !Type.IsAnonymousType();
+			this.TemplateType = IsStrongType ? Type : typeof(ExpandoObject);
+			this.TemplateTypeName = IsStrongType ? Type.Name : "dynamic";
 		}
 	}
-
-    public class ModelTypeInfo<T> : ModelTypeInfo
-    {
-	    public T Value { get; private set; }
-
-	    public ModelTypeInfo(T value) : base(typeof(T))
-	    {
-		    if (value == null)
-		    {
-			    throw new ArgumentNullException(nameof(value));
-		    }
-
-		    this.Value = value;
-	    }
-    }
 }
