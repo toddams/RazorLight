3e7ebd557c8376b58398b6abaa8f5fd1946ce379
user <user@IVAN>
2016-07-29 10:18:28 +0300

    Page context

---
+++ src/RazorLight/PageContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+
+namespace RazorLight
+{
+    public class PageContext
+    {
+		private readonly dynamic viewBag;
+
+	    public PageContext()
+	    {
+			viewBag = new ExpandoObject();
+	    }
+
+		/// <summary>
+		/// Gets the current writer.
+		/// </summary>
+		/// <value>The writer.</value>
+		public TextWriter Writer { get; internal set; }
+
+		/// <summary>
+		/// Gets the view bag.
+		/// </summary>
+		/// <value>The view bag.</value>
+		public dynamic ViewBag => viewBag;
+
+		/// <summary>
+		/// Indicates if current page is being read from file
+		/// If true - 
+		/// </summary>
+	    public bool IsPhysicalPage { get; set; }
+
+	    public string ExecutingFilePath { get; set; }
+	}
+}
