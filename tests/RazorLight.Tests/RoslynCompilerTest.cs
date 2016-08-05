c70f2233582ab4cdb832a0ca10f4ba0a0eeca6d6
user <user@IVAN>
2016-08-05 11:10:58 +0300

    Tests

---
+++ tests/RazorLight.Tests/RoslynCompilerTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using RazorLight.Compilation;
+using RazorLight.Host;
+using RazorLight.Templating;
+using Xunit;
+
+namespace RazorLight.Tests
+{
+    public class RoslynCompilerTest
+    {
+		private CompilationResult? testCompilation;
+		private readonly IMetadataResolver metadataResolver = new UseEntryAssemblyMetadataResolver();
+
+	    private string GetRazorTemplate()
+	    {
+		    var razorCompiler = new DefaultRazorTemplateCompiler();
+			var source = new LoadedTemplateSource("Hello @Model.Title");
+
+		    return razorCompiler.CompileTemplate(new RazorLightHost(null), source);
+	    }
+
+	    private CompilationResult TestCompilation()
+	    {
+		    if (testCompilation == null)
+		    {
+				var compiler = new RoslynCompilerService(metadataResolver);
+				var context = new CompilationContext(GetRazorTemplate(), new HashSet<string>());
+
+			    testCompilation = compiler.Compile(context);
+		    }
+
+		    return testCompilation.Value;
+	    }
+
+		[Fact]
+	    public void Compiler_No_CompilationFailures()
+	    {
+		    CompilationResult compilerResult = TestCompilation();
+
+			Assert.Null(compilerResult.CompilationFailures);
+	    }
+
+		[Fact]
+	    public void Compiler_Template_IsAssignable_From_TemplatePage()
+	    {
+		    var expectedType = typeof(TemplatePage);
+
+			CompilationResult compilerResult = TestCompilation();
+		    var actualType = new DefaultActivator().CreateInstance(compilerResult.CompiledType);
+
+			Assert.IsAssignableFrom(expectedType, actualType);
+		}
+
+		[Fact]
+	    public void Compiler_Throw_On_Corrupted_Template()
+		{
+			var content = "This is a corrupted. @UnknownType should throw";
+			var source = new LoadedTemplateSource(content);
+
+			var razorTemplate = new DefaultRazorTemplateCompiler().CompileTemplate(new RazorLightHost(null), source);
+			var context = new CompilationContext(razorTemplate, new HashSet<string>());
+
+			CompilationResult result = new RoslynCompilerService(metadataResolver).Compile(context);
+			var action = new Action(() => result.EnsureSuccessful());
+
+			Assert.NotNull(result.CompilationFailures);
+			Assert.Throws(typeof(TemplateCompilationException), action);
+		}
+	}
+}
---
+++ tests/RazorLight.Tests/UseEntryAssemblyResolverTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Xunit;
+
+namespace RazorLight.Tests
+{
+    public class UseEntryAssemblyResolverTest
+    {
+		[Fact]
+	    public void Entry_Assembly_Resolver_Has_NonEmpty_Output()
+	    {
+		    var resolver = new UseEntryAssemblyMetadataResolver();
+
+		    IList<MetadataReference> references = resolver.GetMetadataReferences();
+
+			Assert.NotNull(references);
+			Assert.NotEmpty(references);
+	    }
+    }
+}
