62e67dfb9d34428369d4851b2b08ad04b049118a
user <user@IVAN>
2016-08-08 17:43:28 +0300

    Pass custom configuration to EngineFactory

--- src/RazorLight/EngineFactory.cs
+++ src/RazorLight/EngineFactory.cs
@@ -15,13 +15,30 @@ namespace RazorLight
 		/// <param name="root">Root folder where views are stored</param>
 		public static RazorLightEngine CreatePhysical(string root)
 		{
+			return CreatePhysical(root, EngineConfiguration.Default);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="RazorLightEngine"/> that resolves templates by searching 
+		/// them on physical storage with a given <see cref="IEngineConfiguration"/>
+		/// and tracks file changes with <seealso cref="System.IO.FileSystemWatcher"/>
+		/// </summary>
+		/// <param name="root">Root folder where views are stored</param>
+		/// <param name="configuration">Engine configuration</param>
+		public static RazorLightEngine CreatePhysical(string root, IEngineConfiguration configuration)
+		{
 			if (string.IsNullOrEmpty(root))
 			{
 				throw new ArgumentNullException(nameof(root));
 			}
 
+			if (configuration == null)
+			{
+				throw new ArgumentNullException(nameof(configuration));
+			}
+
 			ITemplateManager manager = new FilesystemTemplateManager(root);
-			IEngineCore core = new EngineCore(manager, EngineConfiguration.Default);
+			IEngineCore core = new EngineCore(manager, configuration);
 
 			ICompilerCache compilerCache = new TrackingCompilerCache(root);
 			IPageFactoryProvider pageFactory = new DefaultPageFactory(core.KeyCompile, compilerCache);
@@ -36,15 +53,33 @@ namespace RazorLight
 		/// <param name="rootType">Root type where resource is located</param>
 		public static RazorLightEngine CreateEmbedded(Type rootType)
 		{
+			return CreateEmbedded(rootType, EngineConfiguration.Default);
+		}
+
+		/// <summary>
+		/// Creates a <see cref="RazorLightEngine"/> that resolves templates inside given type assembly as a EmbeddedResource
+		/// with a given <see cref="IEngineConfiguration"/>
+		/// </summary>
+		/// <param name="rootType">Root type where resource is located</param>
+		/// <param name="configuration">Engine configuration</param>
+		public static RazorLightEngine CreateEmbedded(Type rootType, IEngineConfiguration configuration)
+		{
 			ITemplateManager manager = new EmbeddedResourceTemplateManager(rootType);
-			var dependencies = CreateDefaultDependencies(manager);
+			var dependencies = CreateDefaultDependencies(manager, configuration);
 
 			return new RazorLightEngine(dependencies.Item1, dependencies.Item2);
 		}
 
 		private static Tuple<IEngineCore, IPageLookup> CreateDefaultDependencies(ITemplateManager manager)
 		{
-			IEngineCore core = new EngineCore(manager, EngineConfiguration.Default);
+			return CreateDefaultDependencies(manager, EngineConfiguration.Default);
+		}
+
+		private static Tuple<IEngineCore, IPageLookup> CreateDefaultDependencies(
+			ITemplateManager manager, 
+			IEngineConfiguration configuration)
+		{
+			IEngineCore core = new EngineCore(manager, configuration);
 
 			ICompilerCache compilerCache = new DefaultCompilerCache();
 			IPageFactoryProvider pageFactory = new DefaultPageFactory(core.KeyCompile, compilerCache);
--- tests/RazorLight.Tests/EngineFactoryTest.cs
+++ tests/RazorLight.Tests/EngineFactoryTest.cs
@@ -14,5 +14,26 @@ namespace RazorLight.Tests
 
 			Assert.Throws<ArgumentNullException>(action);
 		}
-    }
+
+		[Fact]
+	    public void Ensure_Passed_Configuration_Is_Applided_OnPhysical()
+		{
+			IEngineConfiguration configuration = EngineConfiguration.Default;
+
+			var engine = EngineFactory.CreatePhysical(viewsRoot, configuration);
+
+			Assert.Same(configuration, engine.Configuration);
+		}
+
+		[Fact]
+	    public void Ensure_Passed_Configuration_Is_Applided_OnEmbedded()
+	    {
+			IEngineConfiguration configuration = EngineConfiguration.Default;
+
+		    var engine = EngineFactory.CreateEmbedded(typeof(Sandbox.TestViewModel), configuration);
+
+			Assert.Same(configuration, engine.Configuration);
+		}
+
+	}
 }
