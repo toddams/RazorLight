169e0191c42d7ce2a5097ff18e821a5b4716cc4f
user <user@IVAN>
2016-08-12 11:37:47 +0300

    Edit travis configuration to set views directory on testing environment

--- tests/RazorLight.Tests/PathUtility.cs
+++ tests/RazorLight.Tests/PathUtility.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace RazorLight.Tests
 {
@@ -6,6 +7,13 @@ namespace RazorLight.Tests
     {
         public static string GetViewsPath()
         {
+            string isTesting = Environment.GetEnvironmentVariable("IS_TESTING_ENVIRONMENT");
+
+            if (!string.IsNullOrEmpty(isTesting) && isTesting.Equals("true"))
+            {
+                return Path.Combine(Directory.GetCurrentDirectory(), "tests", "RazorLight.Tests", "Views");
+            }
+
             return Path.Combine(Directory.GetCurrentDirectory(), "Views");
         }
     }
