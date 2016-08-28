887260b7aef40240d65401c1e2168298a6af72fe
user <user@IVAN>
2016-08-28 22:07:19 +0300

    CollectionExtensions configuration action

--- src/RazorLight.MVC/MvcServiceCollectionExtensions.cs
+++ src/RazorLight.MVC/MvcServiceCollectionExtensions.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
 using RazorLight.Templating;
 using RazorLight.Templating.FileSystem;
-using Microsoft.Extensions.DependencyInjection;
 using RazorLight.Caching;
-using Microsoft.AspNetCore.Hosting;
+using RazorLight.Templating.Embedded;
 
 namespace RazorLight.MVC
 {
 	public static class MvcServiceCollectionExtensions
     {
-		public static void AddRazorLight(this IServiceCollection services, string root, IEngineConfiguration configuration)
+		public static void AddRazorLight(this IServiceCollection services, string root)
+		{
+			services.AddRazorLight(root, null);
+		}
+
+		public static void AddRazorLight(this IServiceCollection services, string root, Action<IEngineConfiguration> config)
 		{
 			if (services == null)
 			{
@@ -30,23 +36,58 @@ namespace RazorLight.MVC
 				root = root.Substring(2);
 			}
 
+			//Setup configuration
+			var configuration = EngineConfiguration.Default;
+			config?.Invoke(configuration);
+
+			//Resolve root path 
 			IServiceProvider serviceProvider = services.BuildServiceProvider();
 			IHostingEnvironment env = serviceProvider.GetService<IHostingEnvironment>();
-
 			string absoluteRootPath = System.IO.Path.Combine(env.ContentRootPath, root);
 
-			services.AddSingleton(p => configuration);
 			services.AddTransient<ITemplateManager>(p => new FilesystemTemplateManager(absoluteRootPath));
+			services.AddSingleton<ICompilerCache>(new TrackingCompilerCache(absoluteRootPath));
 
+			services.AddSingleton<IEngineConfiguration>(configuration);
 			services.AddSingleton<IEngineCore, EngineCore>();
 
-			services.AddSingleton<ICompilerCache>(new TrackingCompilerCache(absoluteRootPath));
 			services.AddSingleton<IPageFactoryProvider>(p => new CachingPageFactory(
 				p.GetService<IEngineCore>().KeyCompile, 
 				p.GetService<ICompilerCache>()));
+
 			services.AddSingleton<IPageLookup, FilesystemPageLookup>();
 
 			services.AddSingleton<IRazorLightEngine, RazorLightEngine>();
 		}
-    }
+
+		public static void AddRazorLight(this IServiceCollection services, Type rootType)
+		{
+			services.AddRazorLight(rootType, null);
+		}
+
+		public static void AddRazorLight(this IServiceCollection services, Type rootType, Action<IEngineConfiguration> config)
+		{
+			if (services == null)
+			{
+				throw new ArgumentNullException(nameof(services));
+			}
+
+			if(rootType == null)
+			{
+				throw new ArgumentNullException(nameof(services));
+			}
+
+			var configuration = EngineConfiguration.Default;
+			config?.Invoke(configuration);
+
+			services.AddSingleton(p => configuration);
+			services.AddTransient<ITemplateManager>(p => new EmbeddedResourceTemplateManager(rootType));
+			services.AddSingleton<IEngineCore, EngineCore>();
+
+			services.AddSingleton<IPageFactoryProvider>(p => new DefaultPageFactory(p.GetService<IEngineCore>().KeyCompile));
+			services.AddSingleton<IPageLookup, DefaultPageLookup>();
+
+			services.AddSingleton<IRazorLightEngine, RazorLightEngine>();
+		}
+	}
 }
