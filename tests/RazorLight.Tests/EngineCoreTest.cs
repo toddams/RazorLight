f6d9f4d2e889bca6c0b2248a0afa83e3d7ab1b0a
user <user@IVAN>
2016-09-01 12:22:55 +0300

    RazorLight more tests

---
+++ tests/RazorLight.Tests/EngineCoreTest.cs
@@ -0,0 +1,68 @@
+﻿using Xunit;
+using System;
+using RazorLight;
+using RazorLight.Templating;
+using RazorLight.Templating.Embedded;
+using RazorLight.Compilation;
+using Moq;
+
+namespace RazorLight.Tests
+{
+	public class EngineCoreTest
+    {
+		private IEngineCore testCore => new EngineCore(new EmbeddedResourceTemplateManager(typeof(EngineCoreTest)));
+
+		[Fact]
+		public void Throws_On_Null_Configuration()
+		{
+			Assert.Throws<ArgumentNullException>(() => new EngineCore(new EmbeddedResourceTemplateManager(typeof(EngineCoreTest)), null));
+		}
+
+		[Fact]
+		public void Throws_On_Null_TemplateSource()
+		{
+			Assert.Throws<ArgumentNullException>(() => testCore.CompileSource(null, new ModelTypeInfo(typeof(int))));
+		}
+
+		[Fact]
+		public void Ensure_CompileSource_Returns_Correct_CompilationResult()
+		{
+			var source = new LoadedTemplateSource("Hello, @Model.Title");
+			var model = new { Title = "John" };
+			var modelInfo = new ModelTypeInfo(model.GetType());
+
+			CompilationResult result = testCore.CompileSource(source, modelInfo);
+
+			Assert.NotNull(result);
+			Assert.Null(result.CompilationFailures);
+			Assert.NotNull(result.CompiledType);
+		}
+
+		[Fact]
+		public void Ensure_KeyCompile_Resolved_And_Compiles_Template()
+		{
+			var templateManager = new Mock<ITemplateManager>();
+			templateManager.Setup(t => t.Resolve(It.IsAny<string>())).Returns(new LoadedTemplateSource("Hello @Model.Title"));
+
+			IEngineCore core = new EngineCore(templateManager.Object);
+			CompilationResult result = core.KeyCompile("whatever");
+
+			Assert.NotNull(result);
+			Assert.Null(result.CompilationFailures);
+			Assert.NotNull(result.CompiledType);
+		}
+
+		[Fact]
+		public void Ensure_CompiledType_Is_TypeOf_TemplatePage()
+		{
+			var source = new LoadedTemplateSource("Hello, @Model.Title");
+			var model = new { Title = "John" };
+			var modelInfo = new ModelTypeInfo(model.GetType());
+
+			Type type = testCore.CompileSource(source, modelInfo).CompiledType;
+			var page = Activator.CreateInstance(type);
+
+			Assert.IsAssignableFrom<TemplatePage>(page);
+		}
+    }
+}
---
+++ tests/RazorLight.Tests/PageContextTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Dynamic;
+using Xunit;
+
+namespace RazorLight.Tests
+{
+	public class PageContextTest
+    {
+		[Fact]
+		public void Ensure_ViewBag_Is_Initialized()
+		{
+			var context = new PageContext();
+
+			Assert.NotNull(context.ViewBag);
+		}
+
+		[Fact]
+		public void Ensure_ViewStart_Collection_Is_Initialized()
+		{
+			var context = new PageContext();
+
+			Assert.NotNull(context.ViewStartPages);
+			Assert.Empty(context.ViewStartPages);
+		}
+
+		[Fact]
+		public void Ensure_Passed_Viewbag_Is_Applied()
+		{
+			dynamic viewBag = new ExpandoObject();
+			viewBag.Test = "test";
+
+			var context = new PageContext(viewBag);
+
+			Assert.NotNull(context.ViewBag);
+			Assert.Same(context.ViewBag, viewBag);
+		}
+    }
+}
