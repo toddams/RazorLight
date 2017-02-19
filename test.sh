34ea9e9837c710f7cf167476fd1c5463b8be23c2
user <user@IVAN>
2017-02-19 15:04:49 +0200

    Update travis build script

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 dotnet build */*/project.json
-dotnet test tests/RazorLight.Tests/project.json
-dotnet test tests/RazorLight.MVC.Tests/project.json
+dotnet test tests/RazorLight.Tests/RazorLight.Tests.csproj
+dotnet test tests/RazorLight.MVC.Tests/RazorLight.MVC.Tests.csproj
