a26ab2d14a07cc8f1e8c32fcd22809582d936694
toddams <toddams@gmail.com>
2017-09-06 02:08:30 +0300

    Add custom razor diretives and passes

---
+++ src/RazorLight/Instrumentation/RazorLightAssemblyAttributeInjectionPass.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using System.Diagnostics;
+
+namespace RazorLight.Instrumentation
+{
+    public class RazorLightAssemblyAttributeInjectionPass : IntermediateNodePassBase, IRazorOptimizationPass
+    {
+        private const string RazorLightTemplateAttribute = "global::RazorLight.Razor.RazorLightTemplateAttribute";
+
+        protected override void ExecuteCore(RazorCodeDocument codeDocument, DocumentIntermediateNode documentNode)
+        {
+            var @namespace = documentNode.FindPrimaryNamespace();
+            if (@namespace == null || string.IsNullOrEmpty(@namespace.Content))
+            {
+                // No namespace node or it's incomplete. Skip.
+                return;
+            }
+
+            var @class = documentNode.FindPrimaryClass();
+            if (@class == null || string.IsNullOrEmpty(@class.ClassName))
+            {
+                // No class node or it's incomplete. Skip.
+                return;
+            }
+
+            string generatedTypeName = $"{@namespace.Content}.{@class.ClassName}";
+            string templateKey = codeDocument.Source.FilePath;
+            string escapedTemplateKey = EscapeAsVerbatimLiteral(templateKey);
+
+            string attribute;
+            if (documentNode.DocumentKind == RazorLightTemplateDocumentClassifierPass.RazorLightTemplateDocumentKind)
+            {
+                attribute = $"[assembly:{RazorLightTemplateAttribute}({escapedTemplateKey}, typeof({generatedTypeName}))]";
+            }
+            else
+            {
+                return;
+            }
+
+            int index = documentNode.Children.IndexOf(@namespace);
+            Debug.Assert(index >= 0);
+
+            var pageAttribute = new CSharpCodeIntermediateNode();
+            pageAttribute.Children.Add(new IntermediateToken()
+            {
+                Kind = TokenKind.CSharp,
+                Content = attribute,
+            });
+
+            documentNode.Children.Insert(index, pageAttribute);
+        }
+
+        private static string EscapeAsVerbatimLiteral(string value)
+        {
+            if (value == null)
+            {
+                return "null";
+            }
+
+            value = value.Replace("\"", "\"\"");
+            return $"@\"{value}\"";
+        }
+    }
+}
---
+++ src/RazorLight/Instrumentation/RazorLightTemplateDocumentClassifierPass.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+
+namespace RazorLight.Instrumentation
+{
+    public class RazorLightTemplateDocumentClassifierPass : DocumentClassifierPassBase
+    {
+        public static readonly string RazorLightTemplateDocumentKind = "razorlight.2.0.template";
+
+        protected override string DocumentKind => RazorLightTemplateDocumentKind;
+
+        protected override bool IsMatch(RazorCodeDocument codeDocument, DocumentIntermediateNode documentNode) => true;
+
+        protected override void OnDocumentStructureCreated(
+            RazorCodeDocument codeDocument,
+            NamespaceDeclarationIntermediateNode @namespace,
+            ClassDeclarationIntermediateNode @class,
+            MethodDeclarationIntermediateNode method)
+        {
+            string templateKey = codeDocument.Source.FilePath ?? codeDocument.Source.FilePath;
+
+            base.OnDocumentStructureCreated(codeDocument, @namespace, @class, method);
+
+            @namespace.Content = "RazorLight.CompiledTemplates";
+
+            @class.ClassName = "GeneratedTemplate"; //CSharpIdentifier.GetClassNameFromPath(templateKey);
+            @class.BaseType = "global::RazorLight.TemplatePage<TModel>";
+            @class.Modifiers.Clear();
+            @class.Modifiers.Add("public");
+
+            method.MethodName = "ExecuteAsync";
+            method.Modifiers.Clear();
+            method.Modifiers.Add("public");
+            method.Modifiers.Add("async");
+            method.Modifiers.Add("override");
+            method.ReturnType = $"global::{typeof(System.Threading.Tasks.Task).FullName}";
+        }
+    }
+}
