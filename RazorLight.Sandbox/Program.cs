41eefe1f4f3f9a89a4318eebb64d4a080611be7e
toddams <toddams@gmail.com>
2017-09-06 02:08:42 +0300

    Add sandbox project

---
+++ RazorLight.Sandbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.Language;
+
+namespace RazorLight.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new RazorLightEngine();
+
+            engine.Parse("go.cshtml");
+
+            
+        }
+    }
+}
