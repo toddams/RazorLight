c9bc63ba0a522a38b0b3be11e18f2d4b71eec914
toddams <toddams@gmail.com>
2017-09-10 20:18:54 +0300

    Add nuget properties to project file. Move sandbox project to folder

--- sandbox/RazorLight.Sandbox/Program.cs
+++ sandbox/RazorLight.Sandbox/Program.cs
@@ -5,22 +5,21 @@ namespace RazorLight.Sandbox
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             MainAsync().GetAwaiter().GetResult();
         }
 
         private static async Task MainAsync()
         {
-            var engine = new EngineFactory().ForFileSystem("C:\\");
+            var engine = new EngineFactory().ForEmbeddedResources(typeof(Program));
 
             var model = new
             {
                 Name1 = "qwe"
             };
 
-            string s = await engine.CompileRenderAsync("go.cshtml", model, model.GetType(), null);
-
+            string s = await engine.CompileRenderAsync("go", model, model.GetType(), null);
 
             Console.WriteLine(s);
             Console.ReadKey();
