675bdfa1810c120b3747bd4ba367cfa8900dbd74
toddams <toddams@gmail.com>
2017-09-10 15:41:31 +0300

    Add default caching provider

---
+++ src/RazorLight/Caching/DefaultCachingProvider.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace RazorLight.Caching
+{
+    public class DefaultCachingProvider : ICachingProvider
+    {
+        public DefaultCachingProvider()
+        {
+            var cacheOptions = Options.Create(new MemoryCacheOptions());
+            LookupCache = new MemoryCache(cacheOptions);
+        }
+
+        protected IMemoryCache LookupCache { get; set; }
+
+        public TemplateCacheLookupResult GetTemplate(string key)
+        {
+            if(LookupCache.TryGetValue(key, out TemplateCacheItem template))
+            {
+                var result = new TemplateCacheLookupResult(template);
+
+                return result;
+            }
+            else
+            {
+                return new TemplateCacheLookupResult();
+            }
+        }
+
+        public bool IsTemplateCompiled(string key)
+        {
+            return LookupCache.TryGetValue(key, out _);
+        }
+
+        public void Set(string key, Func<ITemplatePage> pageFactory)
+        {
+            var cacheItem = new TemplateCacheItem(key, pageFactory);
+            LookupCache.Set(key, cacheItem);
+        }
+    }
+}
--- src/RazorLight/Caching/ICachingProvider.cs
+++ src/RazorLight/Caching/ICachingProvider.cs
@@ -4,5 +4,10 @@ namespace RazorLight.Caching
 {
     public interface ICachingProvider
     {
+        TemplateCacheLookupResult GetTemplate(string key);
+
+        void Set(string key, Func<ITemplatePage> pageFactory);
+
+        bool IsTemplateCompiled(string key);
     }
 }
