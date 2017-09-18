e9b275edec96adc82004c72f7e7765a6a7ad1549
toddams <toddams@gmail.com>
2017-09-18 22:00:10 +0300

    Move RenderAsyncDelegate to internal folder

--- src/RazorLight/ITemplatePage.cs
+++ src/RazorLight/ITemplatePage.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Html;
+using RazorLight.Internal;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
--- src/RazorLight/Internal/RenderAsyncDelegate.cs
+++ src/RazorLight/Internal/RenderAsyncDelegate.cs
@@ -1,6 +1,6 @@
 ﻿using System.Threading.Tasks;
 
-namespace RazorLight
+namespace RazorLight.Internal
 {
     public delegate Task RenderAsyncDelegate();
 }
