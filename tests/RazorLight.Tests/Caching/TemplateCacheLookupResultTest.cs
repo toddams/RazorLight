209ae741b0fda394b41af43783cbb5fcedbd6c29
toddams <toddams@gmail.com>
2017-10-23 22:24:55 +0300

    Add more tests

---
+++ tests/RazorLight.Tests/Caching/TemplateCacheLookupResultTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using RazorLight.Caching;
+using Xunit;
+
+namespace RazorLight.Tests.Caching
+{
+    public class TemplateCacheLookupResultTest
+    {
+        [Fact]
+        public void EmptyConstructor_EqualsFailedResult()
+        {
+            var result = new TemplateCacheLookupResult();
+
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public void PassedTemplate_ReturnsSuccessTrue()
+        {
+            var template = Mock.Of<ITemplatePage>();
+
+            var item = new TemplateCacheItem("key", new System.Func<ITemplatePage>(() => template));
+
+            var result = new TemplateCacheLookupResult(item);
+
+            Assert.True(result.Success);
+        }
+    }
+}
