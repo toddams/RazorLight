cfb9aa8e1a1d357bec5dbaa16b5a3353ba20ad85
toddams <toddams@gmail.com>
2017-12-14 22:35:45 +0200

    Rework AddRazorLight extension method

--- sandbox/RazorLight.Sandbox/Program.cs
+++ sandbox/RazorLight.Sandbox/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
+using RazorLight.Extensions;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace RazorLight.Sandbox
 {
@@ -10,9 +15,60 @@ namespace RazorLight.Sandbox
             MainAsync().GetAwaiter().GetResult();
         }
 
+        private static readonly object locker = new object();
+
+        private static int _j;
+        public static int j
+        {
+            get
+            {
+                lock (locker)
+                {
+                    return _j;
+                }
+            }
+            set
+            {
+                lock(locker)
+                {
+                    _j = value;
+                }
+            }
+        }
+
         private static async Task MainAsync()
         {
-            var engine = new EngineFactory().ForFileSystem(@"C:\Projects\RazorLight\sandbox\RazorLight.Sandbox");
+            var engine = new RazorLightEngineBuilder()
+                .UseEmbeddedResourcesProject(typeof(Program))
+                .UseDefaultCachingProvider()
+                .Build();
+
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                ThreadPool.QueueUserWorkItem(async (s) =>
+                {
+                    try
+                    {
+                        results.Add(await engine.CompileRenderAsync("Views.Subfolder.go", null, null, null));
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("J = " + j + " ============================\n" + e.StackTrace);
+                    }
+
+                    j++;
+                });
+            }
+
+            while(j < 100)
+            {
+                Console.WriteLine("Waiting: " + j);
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Finished");
         }
     }
 }
--- src/RazorLight/Extensions/ServiceCollectionExtensions.cs
+++ src/RazorLight/Extensions/ServiceCollectionExtensions.cs
@@ -7,84 +7,26 @@ namespace RazorLight.Extensions
 {
 	public static class ServiceCollectionExtensions
 	{
-		/// <summary>
-		/// Adds RazorLight services that resolve templates from a given <paramref name="root"/>
-		/// </summary>
-		/// <param name="services">Service collection</param>
-		/// <param name="root">Root views folder</param>
-		/// <param name="optionsAction">Configuration (can be null)</param>
-		public static void AddRazorLight(this IServiceCollection services, string root, Action<RazorLightOptions> optionsAction = null)
-		{
-			if (services == null)
-			{
-				throw new ArgumentNullException(nameof(services));
-			}
+        /// <summary>
+        /// Adds RazorLight services that resolve templates from a given <paramref name="root"/>
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        public static void AddRazorLight(this IServiceCollection services, Func<IRazorLightEngine> engineFactoryProvider)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
 
-			if (string.IsNullOrEmpty(root))
-			{
-				throw new ArgumentNullException(nameof(root));
-			}
+            services.AddSingleton<PropertyInjector>();
+            services.AddSingleton<IRazorLightEngine>(p => 
+            {
+                var engine = engineFactoryProvider();
+                AddEngineRenderCallbacks(engine, p);
 
-			if (root.StartsWith("/"))
-			{
-				root = root.Substring(1);
-			}
-			else if (root.StartsWith("~/"))
-			{
-				root = root.Substring(2);
-			}
-
-			//Setup configuration
-			var options = new RazorLightOptions();
-			optionsAction?.Invoke(options);
-
-			//Resolve root path 
-			IServiceProvider serviceProvider = services.BuildServiceProvider();
-			IHostingEnvironment env = serviceProvider.GetService<IHostingEnvironment>();
-			string absoluteRootPath = System.IO.Path.Combine(env.ContentRootPath, root);
-			
-			services.AddSingleton<PropertyInjector>();
-			services.AddSingleton<IRazorLightEngine, RazorLightEngine>(p =>
-			{
-				var engine = new EngineFactory().ForFileSystem(absoluteRootPath, options);
-
-				AddEngineRenderCallbacks(engine, p);
-
-				return engine;
-			});
-		}
-
-		/// <summary>
-		/// Creates RazorLight services that resolves templates inside given type assembly as a EmbeddedResource
-		/// </summary>
-		/// <param name="services">Service collection</param>
-		/// <param name="rootType">Root type where</param>
-		/// <param name="options">Configuration (can be null)</param>
-		public static void AddRazorLight(this IServiceCollection services, Type rootType, Action<RazorLightOptions> optionsAction = null)
-		{
-			if (services == null)
-			{
-				throw new ArgumentNullException(nameof(services));
-			}
-
-			if (rootType == null)
-			{
-				throw new ArgumentNullException(nameof(services));
-			}
-
-			var options = new RazorLightOptions();
-			optionsAction?.Invoke(options);
-
-			services.AddSingleton<PropertyInjector>();
-			services.AddSingleton<IRazorLightEngine, RazorLightEngine>(p =>
-			{
-				var engine = new EngineFactory().ForEmbeddedResources(rootType, options);
-
-				AddEngineRenderCallbacks(engine, p);
-
-				return engine;
-			});
-		}
+                return engine;
+            });
+        }
 
 		private static void AddEngineRenderCallbacks(IRazorLightEngine engine, IServiceProvider services)
 		{
