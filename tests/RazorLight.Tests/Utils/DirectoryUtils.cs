633b34dcbdd1564c0347575a995501ba14660ac2
John Zabroski <johnzabroski@gmail.com>
2021-01-03 23:21:34 -0500

    fix failing linux test

--- tests/RazorLight.Tests/Utils/DirectoryUtils.cs
+++ tests/RazorLight.Tests/Utils/DirectoryUtils.cs
@@ -4,6 +4,14 @@ namespace RazorLight.Tests.Utils
 {
 	public static class DirectoryUtils
 	{
-		public static string RootDirectory => Directory.GetCurrentDirectory();
+		public static string RootDirectory
+		{
+			get
+			{
+				var location = typeof(DirectoryUtils).Assembly.Location;
+				if (!Directory.Exists(location)) throw new DirectoryNotFoundException($"Could not find location [{location}].");
+				return location;
+			}
+		}
 	}
 }
