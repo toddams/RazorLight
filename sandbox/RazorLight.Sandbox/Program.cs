2b0871a9cc4be903297739b0abb3dae9bada61f1
John Zabroski <johnzabroski@gmail.com>
2021-01-05 22:37:18 -0500

    Can't Reproduce #371 - Example 3

--- sandbox/RazorLight.Sandbox/Program.cs
+++ sandbox/RazorLight.Sandbox/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
-using Zstandard.Net;
+using ZstdNet;
 
 namespace RazorLight.Sandbox
 {
 	class Program
 	{
+		static async Task<byte[]> UnwrapStream(DecompressionStream decompressionStream)
+		{
+			var result = new byte[64];
+			await decompressionStream.ReadAsync(result, 0, 64);
+			return result;
+		}
 		public static async Task Main()
 		{
 			var engine = new RazorLightEngineBuilder()
@@ -13,14 +21,37 @@ namespace RazorLight.Sandbox
 				.UseEmbeddedResourcesProject(typeof(Program).Assembly, rootNamespace: "RazorLight.Sandbox.Views")
 				.Build();
 
-			string result = await engine.CompileRenderAsync<object>("Home", null, null);
-			Console.WriteLine(result);
+			//string result = await engine.CompileRenderAsync<object>("Home", null, null);
+			//Console.WriteLine(result);
+
+			await using var tempStream = new MemoryStream();
+			await using var compressionStream = new CompressionStream(tempStream);
+			await GetStream(64).CopyToAsync(compressionStream);
+			await using var decompressionStream = new DecompressionStream(tempStream);
+			await UnwrapStream(decompressionStream);
+			//System.GC.SuppressFinalize(decompressionStream);
+			var model = new {UnwrappedValue = new Lazy<string>(() =>
+			{
+				var x = UnwrapStream(decompressionStream).Result;
+				return Encoding.Default.GetString(x);
+			})};
 
 			string dynamicTemplateResult =
-				await engine.CompileRenderStringAsync("templateKey", "hello, world", new ZstandardDictionary("loremipsum.zdict"));
+				await engine.CompileRenderStringAsync("templateKey", @"@using RazorLight
+@Model.UnwrappedValue.Value", model, null);
 			Console.WriteLine(dynamicTemplateResult);
 
 			Console.WriteLine("Finished");
+		}
+
+		public static MemoryStream GetStream(int length) => new MemoryStream(GetBuffer(length));
+		private static readonly Random Random = new Random(1234);
+		public static byte[] GetBuffer(int length)
+		{
+			var buffer = new byte[length];
+			Random.NextBytes(buffer);
+
+			return buffer;
 		}
 
 		private static readonly object locker = new object();
