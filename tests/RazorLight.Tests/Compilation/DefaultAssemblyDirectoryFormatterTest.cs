5c7f8b9b79a4eff0f7ef464fc933deb1d7853acf
John Zabroski <johnzabroski@gmail.com>
2022-05-19 19:38:59 -0400

    Reproduce #481

---
+++ tests/RazorLight.Tests/Compilation/DefaultAssemblyDirectoryFormatterTest.cs
@@ -0,0 +1,27 @@
+using Microsoft.CodeAnalysis;
+using RazorLight.Compilation;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace RazorLight.Tests.Compilation
+{
+	public class DefaultAssemblyDirectoryFormatterTest
+	{
+    private ITestOutputHelper _testOutputHelper;
+    public DefaultAssemblyDirectoryFormatterTest(ITestOutputHelper testOutputHelper)
+    {
+      _testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+    }
+
+		[Fact]
+		public void Ensure_AdditionalMetadata_IsApplied()
+		{
+			var assembly = typeof(DefaultAssemblyDirectoryFormatterTest).Assembly;
+			var formatter = new DefaultAssemblyDirectoryFormatter();
+      _testOutputHelper.WriteLine(assembly.Location);
+			var directory = formatter.GetAssemblyDirectory(assembly);
+			Assert.NotNull(directory);
+		}
+	}
+}
