360251f98dd6205503b247eea9edffc2a0f45cf0
John Zabroski <johnzabroski@gmail.com>
2022-05-19 20:04:29 -0400

    Reproduce #481

--- tests/RazorLight.Tests/Compilation/DefaultAssemblyDirectoryFormatterTest.cs
+++ tests/RazorLight.Tests/Compilation/DefaultAssemblyDirectoryFormatterTest.cs
@@ -3,23 +3,24 @@ using RazorLight.Compilation;
 using System;
 using System.Collections.Generic;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace RazorLight.Tests.Compilation
 {
 	public class DefaultAssemblyDirectoryFormatterTest
 	{
-    private ITestOutputHelper _testOutputHelper;
-    public DefaultAssemblyDirectoryFormatterTest(ITestOutputHelper testOutputHelper)
-    {
-      _testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
-    }
+		private ITestOutputHelper _testOutputHelper;
+		public DefaultAssemblyDirectoryFormatterTest(ITestOutputHelper testOutputHelper)
+		{
+			_testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+		}
 
 		[Fact]
 		public void Ensure_AdditionalMetadata_IsApplied()
 		{
 			var assembly = typeof(DefaultAssemblyDirectoryFormatterTest).Assembly;
 			var formatter = new DefaultAssemblyDirectoryFormatter();
-      _testOutputHelper.WriteLine(assembly.Location);
+			_testOutputHelper.WriteLine(assembly.Location);
 			var directory = formatter.GetAssemblyDirectory(assembly);
 			Assert.NotNull(directory);
 		}
