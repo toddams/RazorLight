bd7f1029d96cc1dcf3942e77a186dea15621d3cd
Kharitonov, Mark <mark.kharitonov@ceridian.com>
2022-08-13 14:10:01 -0400

    Add unit tests for the precompile command of the RazorLight.Precompile tool.

---
+++ tests/RazorLight.Precompile.Tests/Helper.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Text;
+
+namespace RazorLight.Precompile.Tests
+{
+	internal static class Helper
+	{
+		public static string RunCommandTrimNewline(params string[] args)
+		{
+			var sb = RunCommand(args);
+			sb.Replace("\r\n", "");
+			return sb.ToString();
+		}
+
+		public static StringBuilder RunCommand(params string[] args)
+		{
+			var sw = new StringWriter();
+			Program.ConsoleOut = sw;
+			var exitCode = Program.DoRun(args);
+			Assert.Zero(exitCode);
+			sw.Close();
+
+			return sw.GetStringBuilder();
+		}
+	}
+}
---
+++ tests/RazorLight.Precompile.Tests/PrecompileTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+
+namespace RazorLight.Precompile.Tests
+{
+	public class PrecompileTests
+	{
+		[TestCaseSource(typeof(PrecompileTestCases), nameof(PrecompileTestCases.TestCases))]
+		public void PrecompileFromScratch(string templateFilePath, TestScenario scenario)
+		{
+			scenario.Cleanup();
+
+			var expectedPrecompiledFilePath = GetExpectedPrecompiledFilePath(templateFilePath, scenario);
+			FileAssert.DoesNotExist(expectedPrecompiledFilePath);
+
+			Precompile(templateFilePath, scenario, expectedPrecompiledFilePath);
+
+			scenario.Cleanup();
+			FileAssert.DoesNotExist(expectedPrecompiledFilePath);
+		}
+
+		[TestCaseSource(typeof(PrecompileTestCases), nameof(PrecompileTestCases.TestCases))]
+		public void PrecompileCached(string templateFilePath, TestScenario scenario)
+		{
+			scenario.Cleanup();
+
+			var expectedPrecompiledFilePath = GetExpectedPrecompiledFilePath(templateFilePath, scenario);
+			FileAssert.DoesNotExist(expectedPrecompiledFilePath);
+
+			var sw1 = Stopwatch.StartNew();
+			Precompile(templateFilePath, scenario, expectedPrecompiledFilePath);
+			sw1.Stop();
+
+			FileAssert.Exists(expectedPrecompiledFilePath);
+
+			var sw2 = Stopwatch.StartNew();
+			Precompile(templateFilePath, scenario, expectedPrecompiledFilePath);
+			sw2.Stop();
+
+			TestContext.WriteLine($"TS1 = {sw1.Elapsed}, TS2 = {sw2.Elapsed}");
+
+			scenario.Cleanup();
+			FileAssert.DoesNotExist(expectedPrecompiledFilePath);
+		}
+
+		public static string GetExpectedPrecompiledFilePath(string templateFilePath, TestScenario scenario)
+		{
+			var cacheFileInfo = scenario.ExpectedCachingStrategy.GetCachedFileInfo(scenario.GetTemplateKey(templateFilePath), templateFilePath, scenario.GetExpectedCacheDirectory(templateFilePath));
+			return scenario.GetExpectedPrecompiledFilePath(cacheFileInfo.AssemblyFilePath);
+		}
+
+		public static void Precompile(string templateFilePath, TestScenario scenario, string? expectedPrecompiledFilePath)
+		{
+			var commandLineArgs = new List<string>
+			{
+				"precompile",
+				"-t",
+				templateFilePath
+			};
+			commandLineArgs.AddRange(scenario.ExtraCommandLineArgs);
+
+			var precompiledFilePath = Helper.RunCommandTrimNewline(commandLineArgs.ToArray());
+			Assert.AreEqual(expectedPrecompiledFilePath, precompiledFilePath);
+			FileAssert.Exists(precompiledFilePath);
+		}
+	}
+}
